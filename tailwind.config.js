620321c1a93c28928bfa6beaa2d742c8dd1cbdc0
Javon McGilberry <javonmcgilberry@gmail.com>
2022-06-13 18:50:32 -0400

    new repo

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    important: '#root',
+    variants: {
+        extend: {
+            backgroundColor: ['responsive', 'hover', 'focus', 'active', 'disabled'],
+        },
+    },
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['One Dot'],
+            },
+            colors: {
+                content: '#E5E5E5',
+                'blue-primary': '#002034',
+                'blue-secondary': '#00587C',
+                'blue-accent': '#006491',
+                disabled: '#B5B5B5',
+            },
+        },
+    },
+    plugins: [],
+}
