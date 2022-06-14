c758b8c99ed1024dbabc951f80850b1a2b99517d
Javon McGilberry <javonmcgilberry@gmail.com>
2022-06-13 22:35:24 -0400

    fix tailwind issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { Button } from '@material-tailwind/react';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <Button className="bg-green-500">Button</Button>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code className="bg text-green-500">src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="flex">
+            <aside className="border-box h-screen w-1/5 min-w-[400px] bg-blue-grey-600 p-10">
+                <h1 className="text-4xl text-white">Sidebar</h1>
+            </aside>
+            <main className="h-screen flex-1 overflow-scroll">
+                <div className="h-96 bg-amber-400 p-10">
+                    <h1 className="text-4xl">Top Content</h1>
+                </div>
+                <div className="h-96 bg-white p-10">
+                    <h1 className="text-4xl">Middle Content</h1>
+                </div>
+                <div className="h-96 bg-green-400 p-10">
+                    <h1 className="text-4xl">Middle Content</h1>
+                </div>
+                <div className="h-96 bg-indigo-400 p-10">
+                    <h1 className="text-4xl">Last Content</h1>
+                </div>
+            </main>
         </div>
     );
 }
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+    return (
+        <aside className="bg-slate-700 fixed left-0 top-0 h-screen w-44 p-10">
+            <h1 className="text-4xl text-white">Sidebar</h1>
+        </aside>
+    );
+};
+
+export default Sidebar;
