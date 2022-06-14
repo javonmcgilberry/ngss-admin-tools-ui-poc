1b862e99393615a229b022391b1256ce9ef41698
Javon McGilberry <javonmcgilberry@gmail.com>
2022-06-14 03:18:33 -0400

    add twin macro config

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import { Typography } from '@material-tailwind/react';
 import 'twin.macro';
 import usePageTitle from '../../hooks/usePageTitle';
 
 const Header = () => {
     const currentPage = usePageTitle();
-    return <div tw="border hover:border-black text-blue-400">{currentPage}</div>;
+    return (
+        <Typography variant="h4" tw="border hover:border-black text-blue-400">
+            {currentPage}
+        </Typography>
+    );
 };
 
 export default Header;
