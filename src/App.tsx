31eb90bd735849426d035992231cbcd278e6368d
Javon McGilberry <javonmcgilberry@gmail.com>
2022-06-13 21:53:48 -0400

    typescript fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Button>Button</Button>
+                <Button className="bg-green-500">Button</Button>
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code className="bg text-green-500">src/App.tsx</code> and save to reload.
